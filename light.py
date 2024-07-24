a2aeb8061041342b5868c1c6f2c8dfc88c0d609c
UIO辛可 <2692571183@qq.com>
2024-07-24 18:27:51 +0800

    Fix the bug of negative values appearing in the photosensitive sensor and the bug displaying incorrect values when the soil is dry

--- light.py
+++ light.py
@@ -1,6 +1,5 @@
 from microbit import *
 
-
 class LIGHT(object):
     """基本描述
 
@@ -10,27 +9,30 @@ class LIGHT(object):
         pin: 连接端口
 
     Returns:
-        value: 光线强度 0-16000lux
+        value: 光线强度，单位为勒克斯Lux，范围0-16000
     """
     def __init__(self, pin):
         self.__pin = pin
 
     def get_lightlevel(self):
-        """基本描述
-
-        环境光传感器，返回lux
+        """读取环境光强度并转换为Lux单位
 
         Returns:
-            value: 光强度单位勒克司Lux
+            value: 光强度，单位勒克斯Lux
         """
-        __value = self.__pin.read_analog()
-        if __value <= 200:
-            return ((__value - 45) * (1600 - 0)) / (200 - 45) + 0
-        else:
-            return ((__value - 200) * (14000 - 1600)) / (1023 - 200) + 1600
+        raw_value = self.__pin.read_analog()
 
+        # 为确保结果非负，我们可以直接处理不同区间的线性映射
+        if raw_value <= 200:
+            # 对原始区间[45, 200]进行映射到[0, 1600]
+            lux = (raw_value - 45) * (1600 / (200 - 45))
+        else:
+            # 对原始区间[201, 1023]进行映射到[1600, 14000]
+            lux = 1600 + (raw_value - 200) * ((14000 - 1600) / (1023 - 200))
+
+        return max(0, lux)  # 确保返回值最小为0
 
 if __name__ == "__main__":
-    s = LIGHT(pin1)
+    light_sensor = LIGHT(pin1)
     while True:
-        print(s.get_lightlevel())
+        print(light_sensor.get_lightlevel())
